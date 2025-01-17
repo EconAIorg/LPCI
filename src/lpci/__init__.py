96d34175915b8f0bcf41bc8ab9cdb7f46d710496
Ramón <ramontalvi@gmail.com>
2025-01-17 12:34:36 +0100

    added package version to init2

--- src/lpci/__init__.py
+++ src/lpci/__init__.py
@@ -1,9 +1,8 @@
 # Define the version of the package
 __version__ = "0.1.0"
 
-# Import the main classes/functions from the package
+# Import the main classes from the package
 from .lpci import LPCI
 from .evaluate import EvaluateLPCI
 
-# Define what is accessible when importing *
 __all__ = ['LPCI', 'EvaluateLPCI']
